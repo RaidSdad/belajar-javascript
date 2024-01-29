37be68a317440fe9f4b7e5d6bb45c97474200ba7
RaidSadad <mraidsadad@gmail.com>
2024-01-29 14:06:57 +0700

    pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("Masukan Angka :");
+//alert("Selamat Datang " + nama);
+
+if(x % 2 == 0) {
+    alert(x + " bilangan genap");
+} else if(x % 2 == 1){
+    alert( x + "bilangan ganjil");
+} else{
+    alert("data bukan angka");
+}
